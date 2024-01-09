e6a521030a6c6e9bd0d709dda59c5035eb3ad071
Miourasaki <miourasaki@outlook.com>
2024-01-09 22:22:38 +0800

    更新 App.vue

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -1,65 +1,55 @@
 <template>
-
   <unnamed-proj ref="MainElement">
     <div class="shadow-2xl shadow-[#b40202] max-w-screen-xl sm:mx-auto">
       <router-view />
-
     </div>
-    <Bottom v-if="BottomOpen"/>
+    <Bottom v-if="BottomOpen" />
   </unnamed-proj>
-
 </template>
-
 
 <script setup lang="ts">
 import Bottom from "../../components/Page/Bottom.vue";
-import {onMounted, ref} from "vue";
+import { onMounted, Ref, ref } from "vue";
 
-
-const MainElement = ref(null);
+const MainElement: Ref<HTMLElement | null> = ref(null);
 const BottomOpen = ref(false);
 
-
 onMounted(() => {
+  const container = MainElement.value;
 
-  const FunctionEle = (event) => {
-    const container = MainElement.value;
-    const scrollHeight = container.scrollHeight;
-    const scrollTop = container.scrollTop;
-    const clientHeight = container.clientHeight;
+  const FunctionEle = (event: WheelEvent) => {
+    const scrollHeight = container?.scrollHeight;
+    const scrollTop = container?.scrollTop;
+    const clientHeight = container?.clientHeight;
 
-    if (scrollTop + clientHeight >= scrollHeight) {
-      // 在这里执行你的逻辑
-      if (event.deltaY > 0) {
-        BottomOpen.value = true;
+    if (scrollTop !== undefined && clientHeight !== undefined && scrollHeight !== undefined) {
+      if (scrollTop + clientHeight >= scrollHeight) {
+        if (event.deltaY > 0) {
+          BottomOpen.value = true;
+        }
+      } else if (scrollTop + clientHeight <= scrollHeight -  (document.getElementById("footer")?.scrollHeight || 0)) {
+        if (event.deltaY < 0) {
+          BottomOpen.value = false;
+        }
       }
+    }
+  };
 
+  const FunctionEle2 = () => {
+    const scrollHeight = container?.scrollHeight;
+    const scrollTop = container?.scrollTop;
+    const clientHeight = container?.clientHeight;
 
-    }else if (scrollTop + clientHeight <= scrollHeight -  document.getElementById("footer")?.scrollHeight) {
-      if (event.deltaY < 0) {
+    if (scrollTop !== undefined && clientHeight !== undefined && scrollHeight !== undefined) {
+      if (scrollTop + clientHeight >= scrollHeight - 1) {
+        BottomOpen.value = true;
+      } else if (scrollTop + clientHeight <= scrollHeight - (document.getElementById("footer")?.scrollHeight || 0)) {
         BottomOpen.value = false;
       }
     }
-  }
+  };
 
-  const FunctionEle2 = () => {
-    const container = MainElement.value;
-    const scrollHeight = container.scrollHeight;
-    const scrollTop = container.scrollTop;
-    const clientHeight = container.clientHeight;
-
-    if (scrollTop + clientHeight >= scrollHeight - 1) {
-      // 在这里执行你的逻辑
-      BottomOpen.value = true;
-      console.log("aaaa")
-
-    }else if (scrollTop + clientHeight <= scrollHeight -  document.getElementById("footer")?.scrollHeight) {
-      BottomOpen.value = false;
-    }
-  }
-
-  const container = MainElement.value;
-  container.addEventListener('wheel', FunctionEle);
-  container.addEventListener('scroll', FunctionEle2);
+  container?.addEventListener('wheel', FunctionEle);
+  container?.addEventListener('scroll', FunctionEle2);
 });
 </script>
