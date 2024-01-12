e59a0cff6d673e937a2c39a234dc3ceb50a9c8a9
Miourasaki <miourasaki@outlook.com>
2024-01-12 15:54:18 +0800

    >

---
+++ src/components/auth/box-textarea.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+const props = defineProps(['label','placeholder','method','onInput','icon','maxlength','class']);
+
+
+const InputClass = () => {
+  if (props.method=="none") return "border-stone-400 focus:border-[#b9bcdf]";
+  else if (props.method=="true") return "pr-11 border-[#b9bcdf]";
+  else if (props.method=="false") return "pr-11 border-red-500";
+}
+const TrueClass = () => {
+  if (props.method=="none") return "hidden";
+  else if (props.method=="true") return "text-[#b9bcdf]";
+  else if (props.method=="false") return "text-red-500";
+}
+const True = () => {
+  if (props.method=="none") return "";
+  else if (props.method=="true" && props.icon) return "◯";
+  else if (props.method=="false" && props.icon) return "⨉";
+}
+
+const Model = ref();
+</script>
+
+<template>
+  <div class="relative flex flex-col items-start justify-between  w-full max-w-[25rem]  ">
+    <label class="text-[#797b92] font-mono uppercase mb-0.5">{{props.label}}</label>
+    <textarea v-model="Model" @input="onInput(Model)" class="w-full bg-opacity-10 bg-white border
+          outline-0 px-3 py-4 text-gray-500 leading-5 tracking-wider
+          transition-colors duration-300 transition-opacity
+          focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#b9bcdf]"
+           :class="`${InputClass()} ${props.class}`"
+              :placeholder="props.placeholder" :maxlength="props.maxlength" rows="10">
+
+    </textarea>
+    <label :class="TrueClass()"
+        class="absolute bottom-2.5 text-xl right-4">{{True()}}</label>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/other/SupportView.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+
+import Center from "../../components/Center.vue";
+import BoxInput from "../../components/auth/box-input.vue";
+import BoxButton from "../../components/auth/box-button.vue";
+import {ref} from "vue";
+import {msgApi} from '../../components/api/msg-api.ts';
+
+
+
+
+const CaptchaToken = ref();
+const CaptchaImage = ref();
+const CaptchaMethod = ref("none");
+const Captcha = ref();
+const CaptchaCheck = (value: String) => {
+  Captcha.value = value;
+  if (value) {
+    CaptchaMethod.value = "true";
+  }else CaptchaMethod.value = "false";
+
+}
+
+
+const Title = ref();
+const TitleMethod = ref("none");
+const TitleMCheck = (value: String) => {
+  Title.value = value;
+  if (value) {
+    TitleMethod.value = "true";
+  }else TitleMethod.value = "false";
+
+}
+
+const Content = ref();
+const ContentMethod = ref("none");
+import axios from "axios";
+import BoxTextarea from "../../components/auth/box-textarea.vue";
+
+const ContentCheck = (value: String) => {
+  Content.value = value;
+  if (value) {
+    ContentMethod.value = "true";
+  }else ContentMethod.value = "false";
+
+}
+const SubmitClass = () => {
+  if (TitleMethod.value=="true" && ContentMethod.value=="true") return true;
+  else return false;
+}
+function Submit() {
+  if (TitleMethod.value=="true" && ContentMethod.value=="true") {
+    axios.post("https://api.mio.am/project/unnamed/support",{
+      title: Title.value,
+      content: Content.value,
+    },{
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    }).then(response => {
+      const ApiData = response.data;
+
+      if (ApiData["code"]==1) {
+
+        msgApi("linMsgList","success",ApiData["msg"],5000);
+
+      }else {
+        msgApi("linMsgList","warn",ApiData["msg"],3000);
+      }
+    })
+
+  }
+}
+</script>
+
+<template>
+  <Center class="w-full h-screen px-4 flex-col gap-5">
+    <div class="text-3xl mb-4 uppercase font-mono">Unnamed-Proj. Support</div>
+
+    <box-input :on-input="TitleMCheck" label="Title" placeholder="E.g. There is a bug in this website" type="text" :icon="true" :method="TitleMethod" />
+    <box-textarea :on-input="ContentCheck" label="Content" placeholder="Content" :method="ContentMethod" />
+
+    <div v-if="CaptchaToken" class="flex items-end gap-3 w-full max-w-[25rem]">
+      <div class="flex-grow w-auto">
+        <box-input :on-input="CaptchaCheck" label="Captcha" placeholder="Captcha" type="text" :icon="true" :method="CaptchaMethod" />
+      </div>
+      <img :src="CaptchaImage" alt="Captcha Image" class="h-12 w-auto">
+    </div>
+
+
+
+    <box-button @click="Submit" :method="SubmitClass()" class="mt-8 w-full max-w-[25rem] uppercase">Submit</box-button>
+
+  </Center>
+</template>
